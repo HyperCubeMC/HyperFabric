97b84b5d8c57360d1c4251d911948c5ffcc7f4e5
Justsnoopy30 <justsnoopy30@hypercubemc.cf>
2020-03-29 18:31:27 -0500

    Add startup info.

--- src/main/java/net/hypercubemc/hyperfabric/Mod.java
+++ src/main/java/net/hypercubemc/hyperfabric/Mod.java
@@ -1,6 +1,7 @@
 package net.hypercubemc.hyperfabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class Mod implements ModInitializer {
 	@Override
@@ -8,7 +9,7 @@ public class Mod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		String version = FabricLoader.getInstance().getModContainer("hyperfabric").get().getMetadata().getVersion().getFriendlyString();
+		System.out.println(String.format("Loaded HyperFabric v%s successfilly!",version));
 	}
 }
