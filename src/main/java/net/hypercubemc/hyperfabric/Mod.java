58e7aee53896c15efafb4dcc3463b8e439795e83
Justsnoopy30 <justsnoopy30@hypercubemc.cf>
2020-03-30 18:11:26 -0500

    Add /hyperfabric command!

--- src/main/java/net/hypercubemc/hyperfabric/Mod.java
+++ src/main/java/net/hypercubemc/hyperfabric/Mod.java
@@ -3,6 +3,8 @@ package net.hypercubemc.hyperfabric;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
@@ -17,9 +19,11 @@ import net.minecraft.command.arguments.ColorArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.fabricmc.fabric.api.registry.CommandRegistry;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.text.LiteralText;
 
 public class Mod implements ModInitializer {
 	@Override
@@ -29,22 +33,30 @@ public class Mod implements ModInitializer {
 		// Proceed with mild caution.
 		// The root of the command. This must be a literal argument.
 
+		CommandRegistry.INSTANCE.register(false, this::register);
 		String version = FabricLoader.getInstance().getModContainer("hyperfabric").get().getMetadata().getVersion().getFriendlyString();
 		System.out.println(String.format("Loaded HyperFabric v%s successfully!",version));
 	}
 
-	public static void register(CommandDispatcher<ServerCommandSource> dispatcher){
-			dispatcher.register(literal("broadcast")
-			.requires(source -> source.hasPermissionLevel(2)) // Must be a game master to use the command. Command will not show up in tab completion or execute to non op's or any op that is permission level 1.
-					.then(argument("color", ColorArgumentType.color())
-						.then(argument("message", greedyString())
-							.executes(ctx -> broadcast(ctx.getSource(), getColor(ctx, "color"), getString(ctx, "message")))))); // You can deal with the arguments out here and pipe them into the command.
+	public void register(CommandDispatcher<ServerCommandSource> commandDispatcher) {
+			commandDispatcher.register(CommandManager.literal("hyperfabric")
+					.then(CommandManager.literal("version")
+							// The command to be executed if the command "hyperfabric" is entered with the argument "version"
+							.executes(this::hyperfabric))
+					// The command "hyperfabric" to execute if there are no arguments.
+					.executes(ctx -> {
+						ServerCommandSource source = ctx.getSource();
+						source.sendFeedback(new LiteralText("HyperFabric Help:"), false);
+						source.sendFeedback(new LiteralText("Arguments: version"), false);
+						return 1;
+					})
+			);
 	}
 
-	public static int broadcast(ServerCommandSource source, Formatting formatting, String message) {
-			Text text = new LiteralText(message).formatted(formatting);
-
-			source.getMinecraftServer().getPlayerManager().broadcastChatMessage(text, false);
-			return Command.SINGLE_SUCCESS; // Success
+	public int hyperfabric(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+			ServerCommandSource source = ctx.getSource();
+			String version = FabricLoader.getInstance().getModContainer("hyperfabric").get().getMetadata().getVersion().getFriendlyString();
+			source.sendFeedback(new LiteralText("This server is running Justsnoopy30's HyperFabric Server Mod v" + version), false);
+			return 1;
 	}
 }
