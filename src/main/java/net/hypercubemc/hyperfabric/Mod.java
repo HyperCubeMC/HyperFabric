524ea0619499dee880ee5a99324f45be9ad3b6f3
Justsnoopy30 <justsnoopy30@hypercubemc.cf>
2020-03-31 14:55:34 -0500

    USE JNA (Java Native Access) to enable ansi color codes on windows. This marks HyperFabric 0.1.0 where from now on, I will bump the version numbers appropriately to the semver specification.

--- src/main/java/net/hypercubemc/hyperfabric/Mod.java
+++ src/main/java/net/hypercubemc/hyperfabric/Mod.java
@@ -26,59 +26,38 @@ import net.minecraft.util.Formatting;
 import net.minecraft.text.LiteralText;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import static net.hypercubemc.hyperfabric.ansiCodes.*;
+import com.sun.jna.*;
+import com.sun.jna.platform.win32.WinDef.*;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
 
 public class Mod implements ModInitializer {
+	public void setupAnsiWindows() {
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			// Set output mode to handle virtual terminal sequences
+			Function GetStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");
+			DWORD STD_OUTPUT_HANDLE = new DWORD(-11);
+			HANDLE hOut = (HANDLE) GetStdHandleFunc.invoke(HANDLE.class, new Object[]{STD_OUTPUT_HANDLE});
+
+			DWORDByReference p_dwMode = new DWORDByReference(new DWORD(0));
+			Function GetConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");
+			GetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, p_dwMode});
+
+			int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;
+			DWORD dwMode = p_dwMode.getValue();
+			dwMode.setValue(dwMode.intValue() | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+			Function SetConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");
+			SetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, dwMode});
+		}
+	}
 	Logger log = LogManager.getLogger("hyperfabric");
-	String formatReset = "\033[0m";
-	String formatBold = "\033[1m";
-	String formatLight = "\033[2m";
-	String formatItalic = "\033[3m";
-	String formatUnderline = "\033[4m";
-	String formatBlink = "\033[5m";
-	String formatCrazyBlink = "\033[6m";
-	String formatInvertVideo = "\033[7m";
-	String formatHide = "\033[8m";
-	String formatStrike = "\033[9m";
-	String formatDefaultFont = "\033[10m";
-	String formatFirstAltFont = "\033[11m";
-	String formatSecondAltFont = "\033[12m";
-	String formatFraktur = "\033[20m";
-	String formatDoubleUnderline = "\033[21m";
-	String formatDefaultIntensity = "\033[22m";
-	String formatFrakturItalicOff = "\033[23m";
-	String formatUnderlineOff = "\033[24m";
-	String formatBlinkOff = "\033[25m";
-	String formatInvertVideoOff = "\033[27m";
-	String formatReveal = "\033[28m";
-	String formatStikeOff = "\033[29m";
-	String formatFramed = "\033[51m";
-	String formatCircled = "\033[52m";
-	String formatOverlined = "\033[53m";
-	String formatFramedCircledOff = "\033[54m";
-	String formatOverlinedOff = "\033[55m";
-	String colorBlack = "\033[1;30m";
-	String colorRed = "\033[1;31m";
-	String colorGreen = "\033[1;32m";
-	String colorYellow = "\033[1;33m";
-	String colorBlue = "\033[1;34m";
-	String colorMagenta = "\033[1;35m";
-	String colorCyan = "\033[1;36m";
-	String colorLightGrey = "\033[1;37m";
-	String colorDarkGrey = "\033[1;90m";
-	String colorBrightRed = "\033[1;91m";
-	String colorBrightGreen = "\033[1;92m";
-	String colorBrightYellow = "\033[1;93m";
-	String colorBrightBlue = "\033[1;94m";
-	String colorPink = "\033[1;95m";
-	String colorBrightCyan = "\033[1;96m";
-	String colorWhite = "\033[1;97m";
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		// The root of the command. This must be a literal argument.
-
+		setupAnsiWindows();
 		CommandRegistry.INSTANCE.register(false, this::register);
 		String version = FabricLoader.getInstance().getModContainer("hyperfabric").get().getMetadata().getVersion().getFriendlyString();
 		log.info(colorBlue + "[HyperFabric] Loaded HyperFabric v" + version + " successfully!" + formatReset);
