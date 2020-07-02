5374f4c91c4cf6304a7b4d9a78811247230bec05
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-07-02 12:00:25 -0500

    SetBlock Command Spawn Protection!

--- src/main/java/net/hypercubemc/hyperfabric/mixin/OPSetBlockCommandSpawnProtectionMixin.java
+++ src/main/java/net/hypercubemc/hyperfabric/mixin/OPSetBlockCommandSpawnProtectionMixin.java
@@ -3,32 +3,33 @@ package net.hypercubemc.hyperfabric.mixin;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.block.pattern.CachedBlockPosition;
 import net.minecraft.command.arguments.BlockStateArgument;
-import net.minecraft.server.command.FillCommand;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.command.SetBlockCommand;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Predicate;
 
-@Mixin(FillCommand.class)
-public class OPFillCommandSpawnProtectionMixin {
+@Mixin(SetBlockCommand.class)
+public class OPSetBlockCommandSpawnProtectionMixin {
     private static ServerCommandSource source;
     @Inject(
-            method = "execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/FillCommand$Mode;Ljava/util/function/Predicate;)I",
+            method = "execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/SetBlockCommand$Mode;Ljava/util/function/Predicate;)I",
             at = @At(
                     value = "HEAD",
-                    target = "Lnet/minecraft/server/command/FillCommand;execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/FillCommand$Mode;Ljava/util/function/Predicate;)I"
+                    target = "Lnet/minecraft/server/command/SetBlockCommand;execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/SetBlockCommand$Mode;Ljava/util/function/Predicate;)I"
             )
     )
-    private static void execute(ServerCommandSource commandSource, BlockBox range, BlockStateArgument block, FillCommand.Mode mode, Predicate<CachedBlockPosition> filter, CallbackInfoReturnable<Integer> cir) throws CommandSyntaxException {
+    private static void execute(ServerCommandSource commandSource, BlockPos pos, BlockStateArgument block, SetBlockCommand.Mode mode, Predicate<CachedBlockPosition> condition, CallbackInfoReturnable<Integer> cir) throws CommandSyntaxException {
         source = commandSource;
     }
     @ModifyArg(
-            method = "execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/FillCommand$Mode;Ljava/util/function/Predicate;)I",
+            method = "execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/SetBlockCommand$Mode;Ljava/util/function/Predicate;)I",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/server/world/ServerWorld;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"
@@ -65,7 +66,7 @@ public class OPFillCommandSpawnProtectionMixin {
         }
     }
     @ModifyArg(
-            method = "execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/FillCommand$Mode;Ljava/util/function/Predicate;)I",
+            method = "execute(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/command/arguments/BlockStateArgument;Lnet/minecraft/server/command/SetBlockCommand$Mode;Ljava/util/function/Predicate;)I",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/command/arguments/BlockStateArgument;setBlockState(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;I)Z"
